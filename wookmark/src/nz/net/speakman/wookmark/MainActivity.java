4ad1d2254e7cbcc8fb5e32fa6ff1f87cb6f5f36d
Adam Speakman <adam.speakman@gmail.com>
2012-12-21 20:47:28 -0800

    Basic implementation using sliding menu
    SlidingMenu commit sha is marked dirty. Oh dear. Need to make sure the
    library is cloned in and referenced in eclipse and the
    SlidingFragmentActivity extends SherlockFragmentActivity not regular
    FragmentActivity.

--- wookmark/src/nz/net/speakman/wookmark/MainActivity.java
+++ wookmark/src/nz/net/speakman/wookmark/MainActivity.java
@@ -1,37 +1,49 @@
 package nz.net.speakman.wookmark;
 
-import java.util.ArrayList;
-
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import nz.net.speakman.wookmark.api.WookmarkDownloader;
-import nz.net.speakman.wookmark.images.WookmarkImage;
-
-import android.app.AlertDialog;
-import android.os.AsyncTask;
+import nz.net.speakman.wookmark.fragments.MenuFragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.Window;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends SherlockFragmentActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
-	private ListViewFragment mListViewFragment;
-	private AsyncTask mDownloadTask;
+	private Fragment mContent;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// Request Feature must be called before adding content.
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-		mListViewFragment = new ListViewFragment();
-		ft.add(R.id.mainView, mListViewFragment);
-		ft.commit();
+		
+		mContent = new ListViewFragment(getString(R.string.wookmark_endpoint_default));
+		
+		// set the Above View
+		setContentView(R.layout.content_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, mContent)
+		.commit();
+		
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame, new MenuFragment())
+		.commit();
+		
+        // configure the SlidingMenu
+        SlidingMenu menu = getSlidingMenu();
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
 	}
 
 	@Override
@@ -44,7 +56,11 @@ public class MainActivity extends SherlockFragmentActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.menu_refresh:
-			mDownloadTask = new DownloadImagesTask().execute();
+			if(mContent != null) {
+				if(mContent instanceof RefreshableView) {
+					((RefreshableView)mContent).refresh();
+				}
+			}
 			break;
 		}
 		return super.onOptionsItemSelected(item);
@@ -52,79 +68,23 @@ public class MainActivity extends SherlockFragmentActivity {
 	
 	@Override
 	protected void onDestroy() {
-		if(null != mDownloadTask) {
-			Log.d("WSNZ", "Killing background download task as onDestroy() was called before it returned.");
-			mDownloadTask.cancel(true);
+		if(mContent != null) {
+			Log.d("Wookmark", "Killing background download task as onDestroy() was called before it returned.");
+			if(mContent instanceof RefreshableView) {
+				((RefreshableView)mContent).cancel();
+			}
 		}
 		super.onDestroy();
 	}
+		
+	public void switchContent(Fragment fragment) {
+		mContent = fragment;
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content_frame, fragment)
+		.commit();
+		getSlidingMenu().showContent();
+	}
+
 	
-	private void showResults(ArrayList<WookmarkImage> results) {
-		mListViewFragment.updateImages(results);
-	}
-
-	private class DownloadImagesTask extends
-			AsyncTask<Integer, Void, ArrayList<WookmarkImage>> {
-		/**
-		 * Called before the worker thread is executed. Runs on the UI thread.
-		 */
-		@Override
-		protected void onPreExecute() {
-			// mDownloading = true;
-			// updateRefreshButtonVisibility();
-			setSupportProgress(Window.PROGRESS_END);
-			setSupportProgressBarIndeterminateVisibility(true);
-		}
-
-		/**
-		 * The system calls this to perform work in a worker thread and delivers
-		 * it the parameters given to AsyncTask.execute()
-		 */
-		@Override
-		protected ArrayList<WookmarkImage> doInBackground(Integer... params) {
-			WookmarkDownloader wd = new WookmarkDownloader(
-					new DefaultHttpClient());
-			return wd.getImages("http://www.wookmark.com/api/json/popular");
-		}
-
-		/**
-		 * The system calls this to perform work in the UI thread and delivers
-		 * the result from doInBackground()
-		 */
-		@Override
-		protected void onPostExecute(ArrayList<WookmarkImage> results) {
-			if (isCancelled()) {
-				Log.d("Wookmark",
-						"This download task has been killed. Not updating results.");
-				return;
-			}
-			// mDownloading = false;
-			setSupportProgressBarIndeterminateVisibility(false);
-			// updateRefreshButtonVisibility();
-			if (null == results) {
-				AlertDialog.Builder dialog = new AlertDialog.Builder(
-						MainActivity.this);
-				dialog.setTitle("No Connection")
-						.setMessage(
-								"There appears to be a problem "
-										+ "with the connection. Please make sure "
-										+ "you have internet connectivity.")
-						.setNeutralButton("Close", null);
-				dialog.show();
-				results = new ArrayList<WookmarkImage>();
-			}
-			showResults(results);
-			// mQuakes = results;
-			// Update our "last checked" key in the prefs.
-			// if (results.size() > 0) {
-			// SharedPreferences prefs =
-			// PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-			// Editor editor = prefs.edit();
-			// editor.putString(GeonetService.KEY_PREFS_LAST_CHECKED_ID, results
-			// .get(0).getReference());
-			// editor.commit();
-			// }
-			// updateQuakesDisplay();
-		}
-	}
 }
---
+++ wookmark/src/nz/net/speakman/wookmark/fragments/MenuFragment.java
@@ -0,0 +1,78 @@
+package nz.net.speakman.wookmark.fragments;
+
+import nz.net.speakman.wookmark.ListViewFragment;
+import nz.net.speakman.wookmark.MainActivity;
+import nz.net.speakman.wookmark.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MenuFragment extends ListFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.list, null);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		String[] menuTitles = getResources().getStringArray(R.array.menu_titles);
+		ArrayAdapter<String> menuTitleAdapter = new ArrayAdapter<String>(getActivity(), 
+				android.R.layout.simple_list_item_1, android.R.id.text1, menuTitles);
+		setListAdapter(menuTitleAdapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView lv, View v, int position, long id) {
+		Fragment newContent = null;
+		switch (position) {
+		case 0: // Popular
+			newContent = new ListViewFragment(getString(R.string.wookmark_endpoint_popular));
+			break;
+		case 1: // New
+			newContent = new ListViewFragment(getString(R.string.wookmark_endpoint_new));
+			break;
+		case 2: // User
+			// TODO UserViewFragment
+			//newContent = new UserViewFragment();
+			break;
+		case 3: // Group
+			// TODO GroupViewFragment
+			//newContent = new GroupViewFragment();
+			break;
+		case 4: // Color
+			// TODO ColorViewFragment
+			//newContent = new ColorViewFragment();
+			break;
+		case 5: // Source
+			// TODO SourceViewFragment
+			//newContent = new SourceViewFragment();
+			break;
+		case 6: // Search
+			// TODO SearchViewFragment
+			//newContent = new SearchViewFragment();
+			break;
+		}
+		if (newContent != null)
+			switchFragment(newContent);
+	}
+
+	// the meat of switching the above fragment
+	private void switchFragment(Fragment fragment) {
+		if (getActivity() == null)
+			return;
+		
+		if (getActivity() instanceof MainActivity) {
+			MainActivity ma = (MainActivity) getActivity();
+			ma.switchContent(fragment);
+		}
+	}
+
+
+}
